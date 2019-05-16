f5cde4f40ff6af06b5dba13f9cb31a8bceea3633
ctezna <cteznas@eafit.edu.co>
2019-05-16 11:21:35 -0500

    deploy

--- cocoProject/__init__.py
+++ cocoProject/__init__.py
@@ -50,7 +50,7 @@ if not app.debug:
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-    #return 'en'
+    #return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'en'
 
 from cocoProject import routes, models
