669103269f87eca753bf672d93ed4d98432dfc30
ctezna <cteznas@eafit.edu.co>
2019-07-28 16:55:11 -0500

    deploy

--- cocoProject/routes.py
+++ cocoProject/routes.py
@@ -136,7 +136,7 @@ def connectCoco():
         elif proxy == 801:
             flash(_('Timeout Error. Please try again.'),'danger')
             return redirect(url_for('connectCoco'))
-        print(expiration)
+        print("Expiration:", expiration)
         coco = Coco(name=name, img=img, proxy=proxy, address=address, user_id=user_id)
         db.session.add(coco)
         db.session.commit()
